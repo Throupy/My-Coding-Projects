1838a76b7a639a43aaed742a9237591b8f5b2be1
Owen Throup <38217027+Throupy@users.noreply.github.com>
2018-10-13 17:44:07 +0100

    Add files via upload

---
+++ EMAIL EXTRACTOR/extractor.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+from validate_email import validate_email
+import requests
+import re
+import csv
+urls_to_parse = ["https://github.com/Throupy/GCSE-Computing-Projects/blob/master/README.md",
+                 "https://github.com/Throupy/My-Coding-Projects/blob/master/WORD%20OCCURENCES/README.md"]
+
+def write_data(emails):
+    with open('emails.csv', mode='a') as email_file:
+        email_writer = csv.writer(email_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for email in emails[0]:
+            email_writer.writerow([email])   
+
+def parse(page):
+    emails = [] #Empty set of emails
+    soup = BeautifulSoup(page.content, "html.parser") #parse the page
+    new_emails = list(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", soup.text, re.I)) #find emails with regex
+    print(f"Emails found: ")
+    print(' '.join([x for x in new_emails]))
+    emails.append(new_emails) #add emails to the list
+    for email in emails[0]: #for each email in the set verify the email is valid
+        if validate_email(str(email), verify=True): #verify = true uses DNS to find out if that email actually exists
+            print(f"{email} is valid")
+        else:
+            print(f"{email} isn't valid")
+            emails[0].remove(email)
+
+    write_data(emails)
+
+for url in urls_to_parse:
+    page = requests.get(url) #Set url to page
+    parse(page)
